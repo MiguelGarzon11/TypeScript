817276ca21be5f7ce957fd443e8cfadaf31db1e9
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-01 15:37:38 -0500

    Namespaces

--- TypeScriptModules/Namespaces.ts
+++ TypeScriptModules/Namespaces.ts
@@ -1,11 +1,51 @@
 // Son una forma de agrupar código relacionado en un solo bloque lógico.
 // Se usa con la palabra clave namespace
 
-namespace Matematica {
-    export function suma(a: number, b: number) {
+// Sintaxis 
+
+namespace Matematicas {
+    export function sumar(a: number, b: number): number {
         return a + b;
     }
 }
 
-console.log(Matematica.suma(2, 3));
+// El export es necesario para poder usar la función fuera del namespace.
+
+// Como se usa?
+
+let resultado = Matematicas.sumar(5, 3);
+console.log(resultado); // 8
+
+// Todo está en un solo archivo. Si lo usas en varios archivos, necesitas /// <reference path="..." />
+
+
+
+// Ejercicio: 
+
+namespace Utilidades {
+    export const version: string = "1.0.0";
+
+    export function saludar(nombre: string): void {
+        console.log(`Hola, ${nombre}! Bienvenido a Utilidades v ${version}`);
+    }
+
+    export class Calculadora {
+        sumar(a: number, b: number): number {
+            return a + b;
+        }
+
+        restar(a: number, b: number): number {
+            return a - b;
+        }
+    }
+}
+
+
+// Como usarlo
+
+Utilidades.saludar("Miguel"); // Hola, Miguel! Bienvenido a Utilidades v1.0.0
+
+const calc = new Utilidades.Calculadora();
+console.log(calc.sumar(5,3)); // 8
+console.log(calc.restar(10,4)); // 6
 
