7c5eb5aeb0ae7c19c0122b5bb288f8e41f508fbf
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-01 21:03:29 -0500

    Ecosystem

---
+++ Ecosystem/Ecosystem.ts
@@ -0,0 +1,43 @@
+// En programación, el ecosistema se refiere a todas las herramientas, librerias, utilidades y buneas practicas que se usan alrededor de un lenguaje de programación o entorno. En este caso:
+
+// 1. Formatting (Formateo)
+
+// Se refierer a darle estilo uniforme al código.
+
+// Herramientas populares.
+    // Prettier en JavaScript/TypeScript
+    // Black en Python
+
+// Objetivo que todo el equipo tenga el código bien ordenado y fácil de leer.
+
+
+// 2. Linting
+// El linter analiza el código para encontrar errores o malas prácticas antes de ejecutar.
+
+// Herramientas populares:
+
+    // ESLint en JavaScript/TypeScript
+    // Pylint en Python
+
+// Te ayuda a detectar errores como variables sin usar, estructuras mal hechas, etc.
+
+// 3. Useful Packages (Paquetes Útiles)
+// Son librerías o módulos que puedes instalar para resolver tareas comunes.
+
+// Ejemplos:
+
+    // lodash, axios en JavaScript
+    // requests, numpy en Python
+
+// No tienes que programar todo desde cero, puedes usar estos paquetes.
+
+
+// 4. Build Tools (Herramientas de Construcción)
+// Herramientas que ayudan a preparar tu aplicación para producción.
+
+// Ejemplos:
+
+    // Webpack, Vite, Parcel (JavaScript/TS)
+    // setuptools, pyinstaller (Python)
+
+// Ayudan a combinar archivos, optimizar el código, empaquetar para distribución, etc.
