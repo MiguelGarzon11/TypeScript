5db02aee776da52b1c91e9f850e0e7c6d17b3e56
Miguel Angel <miguelangelgarb@gmail.com>
2025-07-01 20:43:10 -0500

    Global Augmentation

---
+++ TypeScriptModules/GlobalAugmentation.ts
@@ -0,0 +1,8 @@
+// Global Augmentation significa agregar o extender tipos a nivel global en tu proyecto TypeScript, sin importar en qué archivo estés
+
+// Esto se hace cuando quieres:
+// Agregar propiedades personalizadas al objeto Window, global, console, etc.
+// Extender clases globales como Array, String, Promise, etc.
+// Agregar tipos que estarán disponibles globalmente en todo el proyecto.
+
+
